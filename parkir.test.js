3c5d32d8589f64ecbd9296d511a54565c5c5cb1b
lmuhamadaslan <lmuhamadaslan@gmail.com>
2022-11-16 21:29:54 +0800

    initial commit

--- parkir.test.js
+++ parkir.test.js
@@ -1,7 +1,7 @@
 const parkir = require('./parkir.js');
 
-// test case 1 - mobil tiap 1 jam 5000
-test('mobil tiap 1 jam 5000', () => {
+// test case 1 - mobil parkir 1 jam 5000
+test('mobil parkir 1 jam 5000', () => {
   expect(
     parkir(
       'mobil',
@@ -16,8 +16,8 @@ test('mobil tiap 1 jam 5000', () => {
   });
 });
 
-// test case 2 - mobil tiap lebih dari 1 jam 1 menit dibulatkan ke atas
-test('mobil tiap lebih dari 1 menit dibulatkan ke atas', () => {
+// test case 2 - mobil parkir lebih dari 1 jam 1 menit dibulatkan ke atas
+test('mobil parkir lebih dari 1 jam 1 menit dibulatkan ke atas', () => {
   expect(
     parkir(
       'mobil',
@@ -32,8 +32,8 @@ test('mobil tiap lebih dari 1 menit dibulatkan ke atas', () => {
   });
 });
 
-// test case 3 - mobil kalau 1 jam lebih kurang dari 1 menit tidak dibulatkan
-test('mobil kalau 1 jam lebih kurang dari 1 menit tidak dibulatkan', () => {
+// test case 3 - mobil parkir 1 jam lebih kurang dari 1 menit tidak dibulatkan
+test('mobil parkir 1 jam lebih kurang dari 1 menit tidak dibulatkan', () => {
   expect(
     parkir(
       'mobil',
@@ -48,8 +48,8 @@ test('mobil kalau 1 jam lebih kurang dari 1 menit tidak dibulatkan', () => {
   });
 });
 
-// test case 3 - mobil tiap lebih dari 1 hari tambah 80000
-test('mobil tiap lebih dari 1 hari tambah 80000', () => {
+// test case 4 - mobil parkir lebih dari 1 hari tambah 80000
+test('mobil parkir lebih dari 1 hari ditambah 80000', () => {
   expect(
     parkir(
       'mobil',
@@ -64,8 +64,8 @@ test('mobil tiap lebih dari 1 hari tambah 80000', () => {
   });
 });
 
-// test case 4 - motor tiap 1 jam 2000
-test('motor tiap 1 jam 2000', () => {
+// test case 5 - motor parkir 1 jam 2000
+test('motor parkir 1 jam 2000', () => {
   expect(
     parkir(
       'motor',
@@ -80,8 +80,8 @@ test('motor tiap 1 jam 2000', () => {
   });
 });
 
-// test case 5 - motor tiap lebih dari 1 jam 1 menit dibulatkan ke atas
-test('motor tiap lebih dari 1 jam 1 menit dibulatkan ke atas', () => {
+// test case 6 - motor parkir lebih dari 1 jam 1 menit dibulatkan ke atas
+test('motor parkir lebih dari 1 jam 1 menit dibulatkan ke atas', () => {
   expect(
     parkir(
       'motor',
@@ -96,8 +96,8 @@ test('motor tiap lebih dari 1 jam 1 menit dibulatkan ke atas', () => {
   });
 });
 
-// test case 6 - motor kalau 1 jam lebih kurang dari 1 menit tidak dibulatkan
-test('kalau 1 jam lebih kurang dari 1 menit tidak dibulatkan', () => {
+// test case 7 - motor parkir 1 jam lebih kurang dari 1 menit tidak dibulatkan
+test('motor parkir 1 jam lebih kurang dari 1 menit tidak dibulatkan', () => {
   expect(
     parkir(
       'motor',
@@ -109,5 +109,21 @@ test('kalau 1 jam lebih kurang dari 1 menit tidak dibulatkan', () => {
     waktu_masuk: new Date('2022-11-01 12:00:00'),
     waktu_keluar: new Date('2022-11-01 13:00:59'),
     total: 2000,
+  });
+});
+
+// test case 8 - motor parkir lebih dari 1 hari ditambah 40000
+test('motor parkir lebih dari 1 hari ditambah 40000', () => {
+  expect(
+    parkir(
+      'motor',
+      new Date('2022-11-01 12:00:00'),
+      new Date('2022-11-02 13:00:00')
+    )
+  ).toEqual({
+    jenis: 'motor',
+    waktu_masuk: new Date('2022-11-01 12:00:00'),
+    waktu_keluar: new Date('2022-11-02 13:00:00'),
+    total: 42000,
   });
 });
