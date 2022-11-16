2882029b07f621a1d54bf1e3a1d9884055b03d39
lmuhamadaslan <lmuhamadaslan@gmail.com>
2022-11-17 00:39:45 +0800

    initial commit

--- parkir.js
+++ parkir.js
@@ -1,92 +1,96 @@
-function parkir(jenis, masuk, keluar) {
-  // pilih jenis kendaraan
-  if (jenis === 'mobil') {
-    // tiap 1 jam kalau mobil 5000
-    var jam = keluar.getHours() - masuk.getHours();
-    var menit = keluar.getMinutes() - masuk.getMinutes();
-    var detik = keluar.getSeconds() - masuk.getSeconds();
-    var total = jam * 5000;
-
-    // kalau 1 jam lewan 56 detik tidak dibulatkan 
-    // kalau 1 jam 1 menit 2 detik dibulatkan ke atas
-    if (menit === 1 && detik > 1) {
-        total += 5000;
-    } else if (menit > 1) {
-        total += 5000;
-    } else if (menit === 0 && detik < 1) {
-        total = total;
-    } else {
-        total += 0;   
-    }
-    
-
-    // kalau lebih dari 1 hari, tambah 100000
-    if (keluar.getDate() - masuk.getDate() > 0) {
-      total += 80000;
-    } else {
-      total += 0;
-    }
-
-    var data = {
-      jenis: jenis,
-      waktu_masuk: masuk,
-      waktu_keluar: keluar,
-      total_harga: 'Rp.'+total,
-    };
-
-    return data;
-  } else if (jenis === 'motor') {
-    // tiap 1 jam kalau motor 3000
-    var jam = keluar.getHours() - masuk.getHours();
-    var menit = keluar.getMinutes() - masuk.getMinutes();
-    var detik = keluar.getSeconds() - masuk.getSeconds();
-    var total = jam * 2000;
-
-    // kalau 1 jam lewan 56 detik tidak dibulatkan 
-    // kalau 1 jam 1 menit 2 detik dibulatkan ke atas
-    if (menit === 1 && detik > 1) {
-      total += 2000;
-  } else if (menit > 1) {
-      total += 2000;
-  } else if (menit === 0 && detik < 1) {
-      total = total;
-  } else {
-      total += 0;   
+// buat class parkir
+class Parkir {
+  constructor(jenis, masuk, keluar) {
+    this.jenis = jenis;
+    this.masuk = masuk;
+    this.keluar = keluar;
   }
-    
-    // kalau lebih dari 1 hari, tambah 60000
-    if (keluar.getDate() - masuk.getDate() > 0) {
-      total += 40000;
+
+  catatParkir() {
+    var hari = this.keluar.getDate() - this.masuk.getDate();
+    var jam = this.keluar.getHours() - this.masuk.getHours();
+    var menit = this.keluar.getMinutes() - this.masuk.getMinutes();
+    var detik = this.keluar.getSeconds() - this.masuk.getSeconds();
+    var total =
+      this.jenis === 'mobil'
+        ? jam * 5000
+        : this.jenis === 'motor'
+        ? jam * 2000
+        : 0;
+
+    if (this.jenis === 'mobil') {
+      if (menit === 1 && detik > 0) {
+        total += 5000;
+      } else if (menit > 1) {
+        total += 5000;
+      } else if (menit === 1 && detik < 0) {
+        total += total;
+      } else {
+        total += 0;
+      }
+
+      if (this.keluar.getDate() - this.masuk.getDate() > 0) {
+        total += 80000;
+      } else {
+        total += 0;
+      }
+
+      var data = {
+        jenis: this.jenis,
+        waktu_masuk: this.masuk,
+        waktu_keluar: this.keluar,
+        lama_parkir: `${hari} hari ${jam} jam ${menit} menit ${detik} detik`,
+        total_harga: `Rp.${total}`,
+      };
+      return data;
+    } else if (this.jenis === 'motor') {
+      if (menit === 1 && detik > 0) {
+        total += 2000;
+      } else if (menit > 1) {
+        total += 2000;
+      } else if (menit === 1 && detik < 0) {
+        total += total;
+      } else {
+        total += 0;
+      }
+
+      if (this.keluar.getDate() - this.masuk.getDate() > 0) {
+        total += 40000;
+      } else {
+        total += 0;
+      }
+
+      var data = {
+        jenis: this.jenis,
+        waktu_masuk: this.masuk,
+        waktu_keluar: this.keluar,
+        lama_parkir: `${hari} hari ${jam} jam ${menit} menit ${detik} detik`,
+        total_harga: `Rp.${total}`,
+      };
+      return data;
     } else {
-      total += 0;
+      return 'Jenis kendaraan tidak ditemukan';
     }
+  }
 
-    var data = {
-      jenis: jenis,
-      waktu_masuk: masuk,
-      waktu_keluar: keluar,
-      total_harga: 'Rp.'+total,
-    };
-
-    return data;
+  tampilkanData() {
+    console.log(this.catatParkir());
   }
 }
 
-console.log(
-  parkir(
-    'mobil',
-    new Date('2018-01-01 12:00:00'),
-    new Date('2018-01-01 13:01:00')
-  )
+let parkir = new Parkir(
+  'mobil',
+  new Date('2022-11-01 12:00:00'),
+  new Date('2022-11-02 18:00:00')
 );
 
-// motor
-console.log(
-  parkir(
-    'motor',
-    new Date('2018-01-01 12:00:00'),
-    new Date('2018-01-01 13:01:00')
-  )
+let parkir2 = new Parkir(
+  'motor',
+  new Date('2022-11-01 12:00:00'),
+  new Date('2022-11-02 18:00:00')
 );
 
-module.exports = parkir;
+parkir.tampilkanData();
+parkir2.tampilkanData();
+
+module.exports = Parkir;
